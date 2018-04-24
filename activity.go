94922a5624a0c6a0dec1197df4d598df13f87316
Darshan Hire <38034283+dhire05@users.noreply.github.com>
2018-04-24 10:15:28 +0530

    Add files via upload

---
+++ activity.go
@@ -0,0 +1,83 @@
+package CassandraQuery
+
+import (
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+	 "github.com/gocql/gocql"
+	"fmt"
+	
+)
+	// THIS IS ADDED
+	// log is the default package logger which we'll use to log
+	var log = logger.GetLogger("activity-CassaSample")
+
+// MyActivity is a stub for your Activity implementation
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error)  {
+	// Get the activity data from the context
+		clusterIP := context.GetInput("ClusterIP").(string)
+		keySpace := context.GetInput("Keyspace").(string)
+		tableName := context.GetInput("TableName").(string)
+
+	// Use the log object to log the greeting
+	log.Debugf("The Flogo engine says [%s] to [%s] with table [%s]",clusterIP,keySpace,tableName)
+
+	 // Provide the cassandra cluster instance here.
+    cluster := gocql.NewCluster(clusterIP) 
+ 
+    // gocql requires the keyspace to be provided before the session is created.
+    // In future there might be provisions to do this later.
+    cluster.Keyspace = keySpace
+ 
+    // This is time after which the creation of session call would timeout.
+    // This can be customised as needed.
+    //cluster.Timeout = 10 * time.Second 
+ 
+   // cluster.ProtoVersion = 4
+    session, err := cluster.CreateSession()
+	log.Debugf("Session Created Sucessfully")
+	
+	if err != nil {
+       log.Debugf("Could not connect to cassandra cluster: ", err)
+   }
+	log.Debugf("Session : " , session)
+	log.Debugf("Cluster : " , clusterIP)
+	log.Debugf("Keyspace : ", keySpace)
+	log.Debugf("Session Timeout : " ,cluster.Timeout)
+	
+	var(
+		empid int
+		name string
+		salary float64			
+	) 
+	
+	iter := session.Query("SELECT empid, name, salary FROM "+tableName).Iter()
+    for iter.Scan(&empid , &name, &salary) {
+        fmt.Println("EmpID: ", empid,"Name: ", name , "Salary: ", salary)
+        
+    }
+	if err := iter.Close(); err != nil {
+		log.Debugf("Error")		
+	}
+	
+
+	// Set the result as part of the context
+	context.SetOutput("result", "The Flogo engine says Query Connection Successfull")
+
+	// Signal to the Flogo engine that the activity is completed
+	return true, nil
+}
